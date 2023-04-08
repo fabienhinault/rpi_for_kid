06e53997c2c331c1c85a67adf52478a73c756b7a
fabienhinault <fabienhinault@users.noreply.github.com>
2023-04-08 18:16:51 +0200

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -2,13 +2,25 @@
 
 # run as root
 
+set -euf -o pipefail
+
+read -p 'KID=' KID
+
 echo "root's password"
 passwd root
 
-useradd -m kid
-echo "kid's password"
-passwd kid
+for d in {0..9}
+do
+  for u in {0..9}
+  do
+    useradd -m ${KID}$d$u
+    usermod -a -G video,audio ${KID}$d$u
+  done
+done
 
-usermod -a -G video,audio daughter
+echo "${KID}99's password"
+passwd ${KID}99
+
+cp -a /etc/lidghtdm/lightdm.conf /etc/lidghtdm/lightdm.conf~origin
 
 cp -r ./etc /etc
